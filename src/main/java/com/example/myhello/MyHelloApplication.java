ff252c01af86c43f2c98ca9dda9b84042f19733a
whwnddml <junny4u@gmail.com>
2023-08-03 19:41:39 +0900

    Changes

---
+++ src/main/java/com/example/myhello/MyHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myhello/MyHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
